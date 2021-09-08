e139a8e9b578bc85ef5b6942ea3d01f347f9c995
lavijunwal <90325409+lavijunwal@users.noreply.github.com>
2021-09-08 21:38:43 +0530

    Add files via upload

---
+++ src/electricity/Conn.java
@@ -0,0 +1,17 @@
+package electricity;
+import java.sql.*;
+public class Conn {
+
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///project2","root","");    
+            s =c.createStatement();  
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
